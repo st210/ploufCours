75ec38d6e3b7a733d2b9dc4bc4dd3645e3d648dc
Maxime <maxringue@gmail.Com>
2020-03-19 18:54:11 +0100

    ça marche whala

--- src/main/java/com/miage/td/bank/app/entities/Compte.java
+++ src/main/java/com/miage/td/bank/app/entities/Compte.java
@@ -1,19 +1,31 @@
 package com.miage.td.bank.app.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import javax.validation.constraints.Size;
 
-@Document
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Document(collection = "comptes")
 public class Compte {
     @Id
     @Size (max=15)
-    public String id;
+    public long id;
 
-
+    @Field
     public double solde;
 
-    public Long idcclient;
+    @Field
+    public long idclient;
 
 
 
--- src/main/java/com/miage/td/bank/app/repository/CompteRepo.java
+++ src/main/java/com/miage/td/bank/app/repository/CompteRepo.java
@@ -1,10 +1,15 @@
 package com.miage.td.bank.app.repository;
 
 import com.miage.td.bank.app.entities.Compte;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CompteRepo extends CrudRepository<Compte,String> {
+public interface CompteRepo extends MongoRepository<Compte,Long> {
+
+    List<Compte> findAllByIdclient(Long idclient);
 
 }
