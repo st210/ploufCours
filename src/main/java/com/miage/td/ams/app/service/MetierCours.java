df61a842556dd61ba3ebbcb22b347af756cef873
Maxime <maxringue@gmail.Com>
2020-06-25 16:17:16 +0200

    ploufCours : Connexion à mongo get post ok Ajout d'un membre au cours, ça marche mais sais la vérification

--- src/main/java/com/miage/td/ams/app/service/MetierCours.java
+++ src/main/java/com/miage/td/ams/app/service/MetierCours.java
@@ -16,7 +16,7 @@ public class MetierCours {
     private CoursRepo coursRepo;
 
     /**
-     * Ajout d'un cours à un membre
+     * Ajout d'un membre à un cours
      *
      * @param membre membre de l'association à qui associer un cours
      * @return booleen de confirmation du bon ajout du cours à un membre
