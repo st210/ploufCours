b1d88978970e8008da77eaaefce28f8706f53d59
gviala <guillaume.viala@toulouse.miage.fr>
2020-06-25 16:07:35 +0200

    Règle métier participation cours

--- src/main/java/com/miage/td/ams/app/service/MetierCours.java
+++ src/main/java/com/miage/td/ams/app/service/MetierCours.java
@@ -1,11 +1,16 @@
 package com.miage.td.ams.app.service;
 
+import com.fasterxml.classmate.MemberResolver;
 import com.miage.td.ams.app.entities.Cours;
 import com.miage.td.ams.app.repository.CoursRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.lang.reflect.Member;
 
 @Service
 public class MetierCours {
@@ -14,6 +19,11 @@ public class MetierCours {
 
     @Autowired
     private CoursRepo coursRepo;
+
+    private static final String BASE_URL = "http://localhost:10000/clients";
+
+    @Qualifier("http")
+    private RestTemplate http;
 
     /**
      * Ajout d'un cours à un membre
@@ -24,13 +34,25 @@ public class MetierCours {
     public Cours ajouterCours(String idCours, String membre) {
         // todo : ajout d'un cours à un membre
         Cours cours = coursRepo.findById(idCours).get();
+        if (membreValide(membre, cours.niveau)) //Envoyer requêtes de valdidation vers PloufMembres
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Le membre ne peut pas s'inscrire dû à des problèmes admistratifs");
+        }
         if (cours.lstMembre.stream().filter(c->membre.equals(c)).count()==0){
             cours.lstMembre.add(membre);
         }else{
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Le membre est déjà présent au cours");
         }
+        if (cours.lstMembre.stream().count() > 2) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Seul deux personnes peuvent participer");
+        }
 
         return this.coursRepo.save(cours);
+    }
+
+    public boolean membreValide(String membre, int niveau) {
+
+        return http.getForEntity(BASE_URL + "/" + membre + "/verif" + "?niveau=" + niveau, boolean.class).getBody();
     }
 
 }
